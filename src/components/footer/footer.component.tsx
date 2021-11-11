4a412fa690b378c1a50fd596effa5e7b8291c2fc
SuyashKasat <suyashkasat786@gmail.com>
2021-11-12 02:46:47 +0530

    Project

---
+++ src/components/footer/footer.component.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="card-footer text-white mt-auto text-center bg-dark">
+      <h6>Created by Suyash Kasat</h6>
+       <h6>Priyanshi Khetwani</h6>
+            <h6>Utkarsh Jain</h6>
+
+      <p>&copy; Copyright 2021</p>
+    </div>
+  );
+};
+
+export default Footer;
